c167e79931d971711bdadb993eec8b5186935c1a
Siddhant Sahni <siddhantsahni35@gmail.com>
2023-02-10 23:48:50 +0530

    Changing yaml file sourcename

--- stack/pipeline/airbus_pipeline_stack.py
+++ stack/pipeline/airbus_pipeline_stack.py
@@ -22,7 +22,7 @@ class AirbusPipelineStack(Stack):
 
         buildProject = codebuild.PipelineProject(self, 
                                                  "DEV Project", 
-                                                 build_spec=codebuild.BuildSpec.from_object(value="buildspec.yml")
+                                                 build_spec=codebuild.BuildSpec.from_source_filename("buildspec.yml")
                                                  )
 
         # Adding Source Stage to pipeline through CodeStarConnectionsSourceAction
