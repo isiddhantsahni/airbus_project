662b7e4902755bc2ec4838310076ff7d69363be7
Siddhant Sahni <siddhantsahni35@gmail.com>
2023-02-10 23:14:01 +0530

    Adding build stage

--- stack/pipeline/airbus_pipeline_stack.py
+++ stack/pipeline/airbus_pipeline_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     Stack,
     aws_codepipeline as codepipeline,
-    aws_codepipeline_actions
+    aws_codepipeline_actions,
+    aws_codebuild as codebuild
 )
 import aws_cdk
 from constructs import Construct
@@ -13,9 +14,16 @@ class AirbusPipelineStack(Stack):
 
         #Pipeline Definition
 
-        pipeline = codepipeline.Pipeline(self, "Airbus Pipeline", pipeline_name="Airbus Pipeline")
+        pipeline = codepipeline.Pipeline(self, "Airbus Pipeline", pipeline_name="airbus_pipeline")
 
         source_output = codepipeline.Artifact()
+
+        # Defining DEV Stage Project
+
+        buildProject = codebuild.PipelineProject(self, 
+                                                 "DEV Project", 
+                                                 build_spec=codebuild.BuildSpec.from_source_filename(filename="dev.yaml")
+                                                 )
 
         # Adding Source Stage to pipeline through CodeStarConnectionsSourceAction
 
@@ -31,6 +39,16 @@ class AirbusPipelineStack(Stack):
             )]
         )
         
-        
+        # Adding Build Stage
+
+        build_stage = pipeline.add_stage(
+            stage_name="Build",
+            actions=[aws_codepipeline_actions.CodeBuildAction(
+                action_name="Build",
+                project=buildProject,
+                input=source_output,
+                type=aws_codepipeline_actions.CodeBuildActionType.BUILD
+            )]
+        )
 
 
