837d8f43cc2ec267df6b23e3335fd2787d45a382
Siddhant Sahni <siddhantsahni35@gmail.com>
2023-02-16 14:03:56 +0530

    Adding test files

--- tests/unit/test_airbus_project_stack.py
+++ tests/unit/test_airbus_project_stack.py
@@ -1,15 +1,15 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
+# import aws_cdk as core
+# import aws_cdk.assertions as assertions
 
-from airbus_project.airbus_project_stack import AirbusProjectStack
+# from airbus_project.airbus_project_stack import AirbusProjectStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in airbus_project/airbus_project_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = AirbusProjectStack(app, "airbus-project")
-    template = assertions.Template.from_stack(stack)
+# # example tests. To run these tests, uncomment this file along with the example
+# # resource in airbus_project/airbus_project_stack.py
+# def test_sqs_queue_created():
+#     app = core.App()
+#     stack = AirbusProjectStack(app, "airbus-project")
+#     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+# #     template.has_resource_properties("AWS::SQS::Queue", {
+# #         "VisibilityTimeout": 300
+# #     })
