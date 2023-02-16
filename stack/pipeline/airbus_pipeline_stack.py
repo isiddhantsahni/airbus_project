568b5f5ea4ba5242001d15abcc9721ea2b9de54a
Siddhant Sahni <siddhantsahni35@gmail.com>
2023-02-16 15:26:00 +0530

    Adding self-mutate stage

--- stack/pipeline/airbus_pipeline_stack.py
+++ stack/pipeline/airbus_pipeline_stack.py
@@ -46,15 +46,30 @@ class AirbusPipelineStack(Stack):
         )
         
         # Adding Build Stage
-
+        build_output = codepipeline.Artifact("BuildOutput")
         build_stage = pipeline.add_stage(
             stage_name="Build",
             actions=[aws_codepipeline_actions.CodeBuildAction(
                 action_name="Build",
                 project=buildProject,
                 input=source_output,
+                outputs=[build_output],
                 # type=aws_codepipeline_actions.CodeBuildActionType.BUILD
             )]
+        )
+
+        # Adding self-mutate Stage
+
+        update_stage = aws_codepipeline_actions.CloudFormationCreateUpdateStackAction(
+            action_name="Self-Mutate",
+            template_path=source_output.at_path("template.yml"),
+            stack_name="AirbusPipelineStack",
+            admin_permissions=True,
+        )
+
+        self_mutate_stage = pipeline.add_stage(
+            stage_name="Self-Mutate",
+            actions=[update_stage]
         )
         
         buildProject.add_to_role_policy(iam.PolicyStatement(
@@ -70,6 +85,7 @@ class AirbusPipelineStack(Stack):
                      'ssm:GetParameter',
                      's3:*',
                      'iam:PassRole',
+                     'ec2:*'
                      ],
         ))
 
